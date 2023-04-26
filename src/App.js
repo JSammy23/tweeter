58e811183d59068488f378b5191fbb2a70671e24
Jordan <jordansampson23@gmail.com>
2023-04-26 12:05:27 -0500

    Routes and User auth set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import LoginPage from "./pages/LoginPage/LoginPage";
+import FeedPage from "./pages/HomeFeed/FeedPage";
+import { useEffect, useState } from "react";
+import auth from "./services/auth";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user)
+    });
+    return unsubscribe;
+  }, []);
+
+
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/home" /> : <LoginPage />} />
+        <Route path="/home" element={<FeedPage />} />
+      </Routes>
     </div>
   );
 }
