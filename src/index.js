0202e5086e20ebbbd2f18f2454d3136637f4a756
Jordan <jordansampson23@gmail.com>
2023-04-26 12:05:42 -0500

    BrowserRouter added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from "styled-components";
 import theme from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
