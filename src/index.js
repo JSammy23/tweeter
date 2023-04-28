6bbde2e66cef5a061021273513acde59d403aed8
Jordan <jordansampson23@gmail.com>
2023-04-28 12:45:53 -0500

    Add AppProvider

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import { ThemeProvider } from "styled-components";
 import theme from './styles/theme';
 import { BrowserRouter } from 'react-router-dom';
+import { AppProvider } from 'services/appContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AppProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AppProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
