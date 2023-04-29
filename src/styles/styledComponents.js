642eddecd41e64ffb63beb39a64fa50fe4ae2ba9
Jordan <jordansampson23@gmail.com>
2023-04-29 12:32:25 -0500

    Export styles

--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -20,3 +20,43 @@ margin-right: auto;
 margin-left: auto;
 /* border: 2px solid red; */
 `;
+
+export const Title = styled.h2`
+ color: ${props => props.theme.colors.primary};
+ font-weight: bold;
+ margin-left: .7em;
+`;
+
+export const UserHandle = styled.h3`
+ color: ${props => props.theme.colors.secondary};
+ margin-left: .7em;
+`;
+
+export const Button = styled.button`
+ background-color: ${props => props.theme.colors.primary};
+ font-size: ${props => props.fontSize || '1em'};
+ font-weight: bold;
+ padding: .5em;
+ margin: .7em;
+ border-radius: 10px;
+ border: none;
+ outline: none;
+ cursor: pointer;
+
+ &:hover {
+     background-color: ${props => props.theme.colors.accent};
+ }
+`;
+
+export const Module = styled.div`
+ position: absolute;
+ top: 30%;
+ left: 50%;
+ width: 600px;
+ transform: translate(-50%, -50%);
+ box-shadow: 0 15px 25px rgba(0,0,0,.6);
+ border-radius: 10px;
+ padding: 35px;
+ z-index: 999;
+ background-color: ${props => props.theme.colors.bgDark};
+`;
