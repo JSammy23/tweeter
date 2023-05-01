aa9a6d34861f9623e20b531abd76c6e1a5d1617c
Jordan Sampson <jordansampson23@gmail.com>
2023-04-30 21:59:22 -0500

    Add fire storage

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,6 +1,10 @@
 import app from "../firebase.config";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
 
 const db = getFirestore(app);
+
+export const storage = getStorage(app);
+
 
 export default db;
