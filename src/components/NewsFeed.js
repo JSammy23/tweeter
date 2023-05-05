a4a9ebf8ce7c4e714b3ad4b97316985cee85a9c4
Jordan <jordansampson23@gmail.com>
2023-05-05 12:36:10 -0500

    fetchTweets fn for explore filter added.

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, limit, orderBy, query, where } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from 'services/appContext';
 import db from 'services/storage';
@@ -21,20 +21,34 @@ const NewsFeed = ({ user }) => {
     const { activeFilter, setActiveFilter } = useContext(AppContext);
     const [tweets, setTweets] = useState([]);
     
+    const tweetsRef = collection(db, 'tweets');
+
+    // Fetch user tweets
+    const fetchUserTweets = async () => {
+        const userTweetsQuery = query(tweetsRef, where('authorID', '==', user.uid), orderBy('date', 'desc'), limit(50));
+        const userTweetsSnapshot = await getDocs(userTweetsQuery);
+        const userTweetsData = userTweetsSnapshot.docs.map((doc) => doc.data());
+        setTweets(userTweetsData);
+        console.log(userTweetsData);
+    };
+
+    // Fetch all tweets for explore page
+    const fetchTweets = async () => {
+        const tweetsQuery = query(tweetsRef, orderBy('date', 'desc'), limit(100));
+        const tweetsSnapshot = await getDocs(tweetsQuery);
+        const tweetsData = tweetsSnapshot.docs.map((doc) => doc.data());
+        setTweets(tweetsData);
+    }
+
+
     useEffect(() => {
-        // Fetch user tweets
-        const fetchUserTweets = async () => {
-            const userTweetsRef = collection(db, 'tweets');
-            const userTweetsQuery = query(userTweetsRef, where('authorID', '==', user.uid));
-            const userTweetsSnapshot = await getDocs(userTweetsQuery);
-            const userTweetsData = userTweetsSnapshot.docs.map((doc) => doc.data());
-            setTweets(userTweetsData);
-            console.log(userTweetsData);
-        };
 
         if (activeFilter === 'profile') {
             fetchUserTweets();
-        };
+        }
+        else if (activeFilter === 'explore') {
+            fetchTweets();
+        }
 
     }, [activeFilter]);
 
@@ -43,7 +57,7 @@ const NewsFeed = ({ user }) => {
   return (
     <Container>
         {tweets.map((tweet) => (
-            <Tweet key={tweet.id} tweet={tweet} />
+            <Tweet key={tweet.date} tweet={tweet} />
         ))}
     </Container>
   )
