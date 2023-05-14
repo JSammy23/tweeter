e0a6c5f78cf60794c5b30008e97ed08edbf2333c
Jordan Sampson <jordansampson23@gmail.com>
2023-05-14 10:30:35 -0500

    add overlay inside component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,31 +3,33 @@ import './Loading.styles.css';
 
 const Loading = () => {
   return (
-    <div className="background">
-        <div className="boxes">
-            <div className="box">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className="box">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className="box">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className="box">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+    <div className="overlay">
+        <div className="background">
+            <div className="boxes">
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="box">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </div>
     </div>
