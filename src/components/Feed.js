7160c98b279ccdb6fe040341dbcaf2b333be92d1
Jordan <jordansampson23@gmail.com>
2023-05-20 09:39:28 -0500

    Hide scoll bar.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -20,6 +20,32 @@ const FeedContainer = styled.div`
  border-right: 1px solid;
  border-left: 1px solid;
  border-color: ${props => props.theme.colors.secondary};
+
+ /* Hide the scrollbar */
+ &::-webkit-scrollbar {
+    width: 0em;
+    background-color: transparent;
+  }
+
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background-color: transparent;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  /* Hide the scrollbar in Mozilla Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: transparent transparent;
+
+  /* Track */
+  scrollbar-track-color: transparent;
+
+  /* Handle */
+  scrollbar-thumb-color: transparent;
 `;
 
 
