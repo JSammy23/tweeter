2f5131cd726c1d38cb337da5c0c0ebedfc4f985f
Jordan <jordansampson23@gmail.com>
2023-06-08 11:36:14 -0500

    LandingPage initial commit.

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import explore from '../../assets/landingPage/explore.png';
+import profile from '../../assets/landingPage/profile.png';
+
+import styled from 'styled-components';
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
