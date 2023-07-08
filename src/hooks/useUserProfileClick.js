10ba085b1de358d9ec2ab97f73505f9b8a0dfded
Jordan <jordansampson23@gmail.com>
2023-07-08 13:44:30 -0500

    Intiate useUserProfileClick custom hook.

---
+++ src/hooks/useUserProfileClick.js
@@ -0,0 +1,27 @@
+import React, {  useContext } from 'react';
+import { AppContext } from 'services/appContext';
+import { doc, getDoc } from 'firebase/firestore';
+import db from 'services/storage';
+
+export const useUserProfileClick = () => {
+    const { setActiveFilter, setViewedUser, setIsUserLoaded } = useContext(AppContext);
+
+    const handleUserProfileClick = async (userID) => {
+        try {
+            const userDocRef = doc(db, 'users', userID);
+            const userDocSnap = await getDoc(userDocRef);
+
+            if (userDocSnap.exists()) {
+                setViewedUser(userDocSnap.data());
+                setIsUserLoaded(true);
+                setActiveFilter('viewUser');
+            } else {
+                console.log('No such document!');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return handleUserProfileClick;
+};
