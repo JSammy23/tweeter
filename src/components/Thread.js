dab7b63ce3971dcb1098ca3175d2f5127317c596
Jordan <jordansampson23@gmail.com>
2023-07-11 11:58:28 -0500

    Add back arrow btn.

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,13 +1,29 @@
 import React, { useContext } from 'react'
 import { AppContext } from 'services/appContext'
 import Tweet from './Tweet';
+import Compose from './Compose';
 
 import styled from 'styled-components';
 import { Header } from 'styles/styledComponents';
-import Compose from './Compose';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/fontawesome-free-solid';
 
 
-const Thread = () => {
+const StyledIcon = styled(FontAwesomeIcon)`
+ cursor: pointer;
+ color: inherit;
+ font-size: 1.4em;
+ margin: .3em 0;
+
+ &:hover {
+  color: ${props => props.theme.colors.primary};
+ }
+`;
+
+// TODO:
+// Back click is not working, prevFilter in Feedpage not working.
+
+const Thread = ({ onBackClick }) => {
     const { activeThread, setActiveThread, setActiveFilter, currentUser } = useContext(AppContext);
     
 
@@ -15,6 +31,7 @@ const Thread = () => {
     <>
         <Header>
             <h2>Tweet</h2>
+            <StyledIcon icon={faArrowLeft} onClick={onBackClick} />
         </Header>    
         <Tweet key={activeThread} tweet={activeThread} />
         <Compose 
