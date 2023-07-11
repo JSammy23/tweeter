ce0d42b095e5c0db9bb939734750860ad9bb4025
Jordan <jordansampson23@gmail.com>
2023-07-11 01:40:34 -0500

    Add Header from styledComps.

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -2,12 +2,18 @@ import React, { useContext } from 'react'
 import { AppContext } from 'services/appContext'
 import Tweet from './Tweet';
 
+import styled from 'styled-components';
+import { Header } from 'styles/styledComponents';
+
 const Thread = () => {
     const { activeThread, setActiveThread, setActiveFilter } = useContext(AppContext);
     
 
   return (
     <>
+        <Header>
+            <h2>Tweet</h2>
+        </Header>    
         <Tweet key={activeThread} tweet={activeThread} />
     </>
   )
