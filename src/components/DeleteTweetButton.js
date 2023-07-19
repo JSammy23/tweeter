5ef33d9a886f5675dcb159fddd5b314750268979
Jordan <jordansampson23@gmail.com>
2023-07-19 10:09:54 -0500

    Handle delete reply & ref on thread.

--- src/components/DeleteTweetButton.js
+++ src/components/DeleteTweetButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import db from 'services/storage';
-import { doc, deleteDoc } from 'firebase/firestore';
+import { doc, deleteDoc, collection, where, query, getDocs, updateDoc, increment } from 'firebase/firestore';
 
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -25,12 +25,38 @@ const StyledIcon = styled(FontAwesomeIcon)`
  margin-right: .3em;
 `;
 
-const DeleteTweetButton = ({ tweetID }) => {
+const DeleteTweetButton = ({ tweet, isReply, setReplies }) => {
 
     const handleDeleteTweet = async () => {
+      if (isReply) {
+        try {
+          // Delete reply from firestore
+          const replyRef = doc(db, 'replies', tweet.ID);
+          await deleteDoc(replyRef);
+          console.log('Reply deleted.');
+
+          // Remove reply refernece from thread
+          const threadRef = doc(db, tweet.threadType === 'reply' ? 'replies' : 'tweets', tweet.threadID);
+          const repliesSubCollectionRef = collection(threadRef, 'replies');
+          const replyQuery = query(repliesSubCollectionRef, where('replyID', '==', tweet.ID));
+          const replyQuerySnapshot = await getDocs(replyQuery);
+          if (!replyQuerySnapshot.empty) {
+            const replyDoc = replyQuerySnapshot.docs[0];
+            await deleteDoc(replyDoc.ref);
+            await updateDoc(threadRef, {
+              replies: increment(-1),
+            });
+            setReplies(prevReplies => prevReplies.filter(reply => reply.ID !== tweet.ID));
+            console.log('Reply ref removed from thread.');
+          };
+
+        } catch (error) {
+          console.error('Error deleting reply:', error);
+        };
+      } else {
         try {
           // Delete tweet from firestore tweets collection
-          const tweetRef = doc(db, 'tweets', tweetID);
+          const tweetRef = doc(db, 'tweets', tweet.ID);
           await deleteDoc(tweetRef);
           console.log('Tweet deleted');
   
@@ -40,7 +66,8 @@ const DeleteTweetButton = ({ tweetID }) => {
           
         } catch (error) {
           console.error('Error deleting tweet:', error);
-        }
+        };
+      };
     };
 
   return (
