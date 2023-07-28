b8d3a706c8e1c0577cb2a385540f00981a0aa371
Jordan <jordansampson23@gmail.com>
2023-07-28 12:40:42 -0500

    Initial ReplyTweet

---
+++ src/components/ReplyTweet.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import StandardTweet from './StandardTweet';
+import { doc, getDoc } from 'firebase/firestore';
+import db from 'services/storage';
+
+const ReplyTweet = ({ tweet }) => {
+    const [replyToTweets, setReplyToTweets] = useState([]);
+
+    const fetchReplyToTweet = async (tweetId, replyToTweets) => {
+        const tweetDocRef = doc(db, 'tweets', tweetId);
+        const tweetDoc = await getDoc(tweetDocRef);
+
+        if (!tweetDoc.exists) {
+            console.log('No such document!');
+            return null;
+        }
+
+        const tweetData = tweetDoc.data();
+        // Add the tweet to the replyToTweets array
+        replyToTweets.push(tweetData);
+
+        // If the tweet is not a reply to another tweet, return the array
+        if (!tweetData.isReply) {
+            return replyToTweets;
+        }
+
+        // If the tweet is a reply, recursively get reply tweet until top level tweet is fetched
+        return fetchReplyToTweet(tweetData.replyTo, replyToTweets);
+    };
+
+    useEffect(() => {
+        const fetchInitialData = async () => {
+            const replyToTweets = [];
+            const finalReplyToTweets = await fetchReplyToTweet(tweet.replyTo, replyToTweets);
+            setReplyToTweets(finalReplyToTweets);
+        };
+        fetchInitialData();
+    }, [tweet]);
+
+  return (
+    <div>
+        {replyToTweets.map((tweet, index) => (
+            <StandardTweet key={index} tweet={tweet} isMini />
+        ))}
+        <StandardTweet key={replyToTweets.length} tweet={tweet} />
+    </div>
+  )
+}
+
+export default ReplyTweet
