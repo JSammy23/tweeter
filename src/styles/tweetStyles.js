d60eb734e8b3a81313ca444ac4be3e2293efa0a5
Jordan <jordansampson23@gmail.com>
2023-07-28 12:39:27 -0500

    Add isMini styling prop.

--- src/styles/tweetStyles.js
+++ src/styles/tweetStyles.js
@@ -6,6 +6,13 @@ export const TweetCard = styled.div`
  display: flex;
  padding: .5em;
  border-bottom: 1px solid ${props => props.theme.colors.secondary};
+
+ ${props => props.isMini && `
+  width: 90%;
+  padding: .3em;
+  margin-left: auto;
+  border-bottom: none;
+ `}
 `;
 
 export const TweetHeader = styled.div`
@@ -26,11 +33,20 @@ export const UserImage = styled.img`
  border: 1px solid black;
  margin-right: .5em;
  cursor: pointer;
+
+ ${props => props.isMini && `
+  width: 50px;
+  height: 50px;
+ `}
 `;
 
 export const Name = styled.h2`
  color: ${props => props.theme.colors.primary};
  margin-right: .3em;
+
+ ${props => props.isMini && `
+  font-size: 1.2em;
+ `}
 `;
 
 export const Handle = styled.h3`
@@ -41,6 +57,10 @@ export const Handle = styled.h3`
   color: ${props => props.theme.colors.primary};
   text-decoration: underline;
  }
+
+ ${props => props.isMini && `
+  font-size: 1.2em;
+ `}
 `;
 
 export const TweetDate = styled.div`
@@ -56,6 +76,10 @@ export const TweetBody = styled.div`
  color: #fff;
  font-size: 1.3em;
  margin-top: .3em;
+
+ ${props => props.isMini && `
+  font-size: 1.2em;
+ `}
 `;
 
 export const TweetReactions = styled.div`
