97ee94e8680c7c039fa220fa8f849b1fa03211e3
Jordan Sampson <jordansampson23@gmail.com>
2023-07-30 15:21:49 -0500

    TODO comments

--- src/components/Retweet.js
+++ src/components/Retweet.js
@@ -13,6 +13,8 @@ export const StyledIcon = styled(FontAwesomeIcon)`
  cursor: pointer;
 `;
 
+// TODO:
+// Retweeted status is incorrect on user profile after retweeted. active state wrong.
 const Retweet = ({ tweet }) => {
   const [retweets, setRetweets] = useState(tweet.retweets || 0);
   const [retweeted, setRetweeted] = useState(false);
