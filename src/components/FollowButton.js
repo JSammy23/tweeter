b0d662e3235486bc80e1abb277c7ad7dde74057f
Jordan Sampson <jordansampson23@gmail.com>
2023-07-30 14:16:51 -0500

    Combine handleFollow into one function.

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -31,69 +31,130 @@ const FollowButton = ({ user }) => {
       const isFollowing = followingList.some((followingUser) => followingUser.user === user);
       setIsFollowing(isFollowing);
     };
-  
-    const handleFollowUser = async () => {
-      try {
-        setFollowingList((prevFollowingList) => [...prevFollowingList, { user }]);
-        setIsFollowing(true);
-      } catch (error) {
-        console.error('Error adding user to local followList', error);
-      }
-  
-      try {
-        await addDoc(followingRef, { user });
-        console.log('User followed');
-      } catch (error) {
-        console.error('Error following user', error);
-      }
-  
-      try {
-        const followersRef = collection(userToFollowRef, 'followers');
-        await addDoc(followersRef, { user: currentUser.uid });
-        console.log('Current User added to follower list');
-      } catch (error) {
-        console.error('Error adding current user to follower list.', error);
+
+    const handleFollow = async () => {
+      if (isFollowing) {
+        try {
+          setFollowingList((prevFollowingList) => prevFollowingList.filter((item) => item.user !== user));
+          setIsFollowing(false);
+          try {
+            const querySnapshot = await getDocs(followingRef);
+            querySnapshot.forEach((doc) => {
+              if (doc.data().user === user) {
+                deleteDoc(doc.ref);
+                console.log('User unfollowed');
+              }
+            });
+          } catch (error) {
+            console.error('Error unfollowing user', error);
+          }
+      
+          try {
+            const followersRef = collection(userToFollowRef, 'followers');
+            const querySnapshot = await getDocs(followersRef);
+            querySnapshot.forEach((doc) => {
+              if (doc.data().user === currentUser.uid) {
+                deleteDoc(doc.ref);
+                console.log('Current user removed from viewedUser follower list');
+              }
+            });
+          } catch (error) {
+            console.error('Error removing current user from viewedUser follower list', error);
+          }
+          // ...rest of the code to unfollow the user
+        } catch (error) {
+          console.error('Error unfollowing user', error);
+        }
+      } else {
+        try {
+          if (!followingList.some(followingUser => followingUser.user === user)) {
+            setFollowingList((prevFollowingList) => [...prevFollowingList, { user }]);
+            setIsFollowing(true);
+          }
+          
+          try {
+            await addDoc(followingRef, { user });
+            console.log('User followed');
+          } catch (error) {
+            console.error('Error following user', error);
+          }
+      
+          try {
+            const followersRef = collection(userToFollowRef, 'followers');
+            await addDoc(followersRef, { user: currentUser.uid });
+            console.log('Current User added to follower list');
+          } catch (error) {
+            console.error('Error adding current user to follower list.', error);
+          }
+          // ...rest of the code to follow the user
+        } catch (error) {
+          console.error('Error following user', error);
+        }
       }
     };
   
-    const handleUnfollowUser = async () => {
-      try {
-        setFollowingList((prevFollowingList) => prevFollowingList.filter((item) => item.user !== user));
-        setIsFollowing(false);
-      } catch (error) {
-        console.error('Error removing user from local followList', error);
-      }
+    // const handleFollowUser = async () => {
+    //   try {
+    //     setFollowingList((prevFollowingList) => [...prevFollowingList, { user }]);
+    //     setIsFollowing(true);
+    //   } catch (error) {
+    //     console.error('Error adding user to local followList', error);
+    //   }
   
-      try {
-        const querySnapshot = await getDocs(followingRef);
-        querySnapshot.forEach((doc) => {
-          if (doc.data().user === user) {
-            deleteDoc(doc.ref);
-            console.log('User unfollowed');
-          }
-        });
-      } catch (error) {
-        console.error('Error unfollowing user', error);
-      }
+    //   try {
+    //     await addDoc(followingRef, { user });
+    //     console.log('User followed');
+    //   } catch (error) {
+    //     console.error('Error following user', error);
+    //   }
   
-      try {
-        const followersRef = collection(userToFollowRef, 'followers');
-        const querySnapshot = await getDocs(followersRef);
-        querySnapshot.forEach((doc) => {
-          if (doc.data().user === currentUser.uid) {
-            deleteDoc(doc.ref);
-            console.log('Current user removed from viewedUser follower list');
-          }
-        });
-      } catch (error) {
-        console.error('Error removing current user from viewedUser follower list', error);
-      }
-    };
+    //   try {
+    //     const followersRef = collection(userToFollowRef, 'followers');
+    //     await addDoc(followersRef, { user: currentUser.uid });
+    //     console.log('Current User added to follower list');
+    //   } catch (error) {
+    //     console.error('Error adding current user to follower list.', error);
+    //   }
+    // };
+  
+    // const handleUnfollowUser = async () => {
+    //   try {
+    //     setFollowingList((prevFollowingList) => prevFollowingList.filter((item) => item.user !== user));
+    //     setIsFollowing(false);
+    //   } catch (error) {
+    //     console.error('Error removing user from local followList', error);
+    //   }
+  
+    //   try {
+    //     const querySnapshot = await getDocs(followingRef);
+    //     querySnapshot.forEach((doc) => {
+    //       if (doc.data().user === user) {
+    //         deleteDoc(doc.ref);
+    //         console.log('User unfollowed');
+    //       }
+    //     });
+    //   } catch (error) {
+    //     console.error('Error unfollowing user', error);
+    //   }
+  
+    //   try {
+    //     const followersRef = collection(userToFollowRef, 'followers');
+    //     const querySnapshot = await getDocs(followersRef);
+    //     querySnapshot.forEach((doc) => {
+    //       if (doc.data().user === currentUser.uid) {
+    //         deleteDoc(doc.ref);
+    //         console.log('Current user removed from viewedUser follower list');
+    //       }
+    //     });
+    //   } catch (error) {
+    //     console.error('Error removing current user from viewedUser follower list', error);
+    //   }
+    // };
 
     const renderFollowButton = () => {
         if (isFollowing) {
             return (
-                <Button onClick={handleUnfollowUser} >
+                <Button onClick={handleFollow} >
                     Following
                     <Checkmark>
                         <FontAwesomeIcon icon={faCheck} />
@@ -102,7 +163,7 @@ const FollowButton = ({ user }) => {
             )
         } else {
             return (
-                <Button onClick={handleFollowUser}>
+                <Button onClick={handleFollow}>
                     Follow
                 </Button>
             )
