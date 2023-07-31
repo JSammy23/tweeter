d708fbca765086b2532c22fb4715e90cafac0e68
Jordan Sampson <jordansampson23@gmail.com>
2023-07-31 00:45:04 -0500

    Move backend functions out to utilities file.

--- src/utilities/tweetUtilites.js
+++ src/utilities/tweetUtilites.js
@@ -1,12 +1,14 @@
-import { updateDoc, deleteDoc, getDocs, addDoc, arrayRemove, arrayUnion, collection, doc, where, query } from 'firebase/firestore'; 
+import { updateDoc, deleteDoc, getDocs, addDoc, arrayRemove, arrayUnion, collection, doc, where, query, increment } from 'firebase/firestore'; 
 import db from 'services/storage';
+
+// ***********/ Retweet Tweet /**************/
 
 export const getRetweets = async (tweetId, userId) => {
     const userRetweetsRef = collection(db, 'users', userId, 'retweets');
-    const userRetweetsQuery = query(userRetweetsRef, where('ID', '==', tweetId));
+    const userRetweetsQuery = query(userRetweetsRef, where('tweetID', '==', tweetId));
     const userRetweetsSnapshot = await getDocs(userRetweetsQuery);
     return !userRetweetsSnapshot.empty;
-}
+};
 
 export const removeRetweet = async (tweetId, userId, newRetweetCount) => {
     const tweetRef = doc(db, 'tweets', tweetId);
@@ -35,7 +37,7 @@ export const removeRetweet = async (tweetId, userId, newRetweetCount) => {
         const userRetweetsDocRef = doc(db, 'users', userId, 'retweets', userRetweetsDoc.id);
         await deleteDoc(userRetweetsDocRef);
     }
-}
+};
 
 export const addRetweet = async (tweetId, userId, newRetweetCount) => {
     const tweetRef = doc(db, 'tweets', tweetId);
@@ -56,4 +58,52 @@ export const addRetweet = async (tweetId, userId, newRetweetCount) => {
         tweetID: tweetId,
         date: new Date(),
     });
-}
+};
+
+// ***********/ Like Tweet /**************/
+
+export const checkIfLiked = async (tweetId, userId) => {
+    const userLikesRef = collection(db, 'users', userId, 'likes');
+    const userLikesQuery = query(userLikesRef, where('tweetID', '==', tweetId));
+    const userLikesSnapshot = await getDocs(userLikesQuery);
+    return !userLikesSnapshot.empty;
+};
+
+export const likeTweet = async (tweetId, userId) => {
+    try {
+        const documentRef = doc(db, 'tweets', tweetId);
+        const userLikesRef = collection(db, 'users', userId, 'likes');
+        await updateDoc(documentRef, {
+            likedBy: arrayUnion(userId),
+            likes: increment(1)
+        });
+        await addDoc(userLikesRef, {
+            tweetID: tweetId,
+            date: new Date(),
+        });
+        console.log('Tweet liked');
+    } catch (error) {
+        console.error('Error liking tweet', error);
+    }
+};
+
+export const unlikeTweet = async (tweetId, userId) => {
+    try {
+        const documentRef = doc(db, 'tweets', tweetId);
+        const userLikesRef = collection(db, 'users', userId, 'likes');
+        await updateDoc(documentRef, {
+            likedBy: arrayRemove(userId),
+            likes: increment(-1)
+        });
+        const userLikesQuery = query(userLikesRef, where('tweetID', '==', tweetId));
+        const userLikesSnapshot = await getDocs(userLikesQuery);
+        const userLikesDoc = userLikesSnapshot.docs[0];
+        if (userLikesDoc) {
+            const userLikesDocRef = doc(userLikesRef, userLikesDoc.id);
+            await deleteDoc(userLikesDocRef);
+        }
+        console.log('Tweet unliked');
+    } catch (error) {
+        console.error('Error unliking tweet', error);
+    }
+};
