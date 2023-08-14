950ede1cdfc8eac4dc971f9b16d8969f724a4386
Jordan <jordansampson23@gmail.com>
2023-08-14 13:01:35 -0500

    New nested routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import LoginPage from "pages/LoginPage/LoginPage";
-import FeedPage from "pages/HomeFeed/FeedPage";
+import FeedPage from "pages/Feed/FeedPage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignUpPage from "pages/SignUp/SignUpPage";
 import PrivateRoute from "components/PrivateRoute";
+import Compose from "components/Compose";
+import UserProfile from "components/UserProfile";
+import { useContext } from "react";
+import { AppContext } from "services/appContext";
 
 function App() {
-
+  const { currentUser } = useContext(AppContext);
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route
-        path="home"
-        element={
-          <PrivateRoute>
-            <FeedPage />
-          </PrivateRoute>
-        } /> 
         <Route path="/signup" element={<SignUpPage />} />
+        <Route 
+          path="/*"
+          element={
+            <PrivateRoute>
+              <FeedPage>
+                <Routes>
+                <Route path="/home" element={<Compose/>} />
+                <Route path="/profile" element={<UserProfile userUid={currentUser.uid} />} />
+                </Routes>
+              </FeedPage>
+            </PrivateRoute>
+          } />
       </Routes>
     </div>
   );
