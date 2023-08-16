26444e725e65354a2e9b861794a258a2616460d0
Jordan <jordansampson23@gmail.com>
2023-08-16 13:44:05 -0500

    grab user tweets and likes on render

--- src/components/TweetFetcher.js
+++ src/components/TweetFetcher.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import Tweet from './Tweet';
 import { fetchFromFirestore } from 'utilities/firebaseUtils';
-import { fetchTweetBucket } from 'utilities/tweetUtilites';
+import { fetchFromUserSubCollection } from 'utilities/tweetUtilites';
 
-const TweetFetcher = ({ userUid }) => {
+const TweetFetcher = ({ userUid, showLikes }) => {
 
-    const [data, setData] = useState([]);
+    const [userTweetBucket, setUserTweetBucket] = useState([]);
+    const [userLikes, setUserLikes] = useState([]);
 
     useEffect(() => {
         const fetchTweets = async () => {
             try {
-                const tweets = await fetchTweetBucket(userUid);
-                console.log(tweets); // To console the fetched tweets
-                setData(tweets); // To set the fetched tweets to the data state
+                const userTweets = await fetchFromUserSubCollection(userUid, 'tweetBucket');
+                setUserTweetBucket(userTweets); // To set the fetched tweets to the data state
+                const userLikes = await fetchFromUserSubCollection(userUid, 'likes');
+                setUserLikes(userLikes);
             } catch (error) {
                 console.error("Error fetching tweets:", error);
             }
@@ -22,11 +24,21 @@ const TweetFetcher = ({ userUid }) => {
     }, [userUid]); 
 
     const mapTweetsToComponents = (tweets) => {
-        return tweets.map((tweet) => <Tweet key={tweet.id} tweet={tweet} />);
+        if (tweets.length === 0) {
+            return;
+        } else {
+            return tweets.map((tweet) => <Tweet key={tweet.id} tweet={tweet} />);
+        }
     };
 
     const renderTweets = () => {
-        return mapTweetsToComponents(data);
+        let tweets;
+        if (showLikes) {
+            tweets = userLikes;
+        } else {
+            tweets = userTweetBucket
+        }
+        return mapTweetsToComponents(tweets);
     };
 
   return (
